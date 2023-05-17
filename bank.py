d39e67f177d21956cd4c5d373460648fcc7f1fb5
Yedamit <jatarazona39@yahoo.com>
2023-05-17 17:59:04 -0500

    5:58 pm YEDa

--- bank.py
+++ bank.py
@@ -8,3 +8,23 @@ import random
 from datetime import datetime
 from os.path import exist
 
+# ─── Constantes ────────────────────────────────────────────────────────────────────
+
+FILE_PATH =  "bank.json"   
+# ─── Date Base ────────────────────────────────────────────────────────────────────
+
+def get_data ():
+    """
+    mira la info del data
+    """
+    # Si no hay fila vacia
+    if not exists(FILE_PATH):
+            return {}
+f = open(FILE_PATH, "r")
+data = f.read()
+
+#convierte string json en objeto de python
+return json.loads(data)
+
+
+
